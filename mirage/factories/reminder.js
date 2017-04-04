131d6fbf2c8933b0aca2bc961b9213dbe6d213ff
Marissa Reinke <reinke.marissa@gmail.com>
2017-04-04 10:09:30 -0600

    Initial commit that displays Remember Items Adds reminder model Adds route reminder with template reminder Add a root route Creates a test that look at displaying reminders

--- mirage/factories/reminder.js
+++ mirage/factories/reminder.js
@@ -1,7 +1,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title: () => faker.lorem.words().join(' ').capitalize(),
+  title: () => faker.lorem.words().capitalize(),
   date: () => faker.date.recent(3),
-  notes: () => faker.lorem.paragraph(),
+  body: () => faker.lorem.paragraph(),
 });
