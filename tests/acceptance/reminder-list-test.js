bea6dd7541cc43b7253425b7d3e68453b28e41f5
Marissa Reinke <reinke.marissa@gmail.com>
2017-04-04 18:33:12 -0600

    Create test for new reminder

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -1,6 +1,5 @@
 /* globals server */
-
-import { test, skip } from 'qunit';
+import { test, /*skip*/ } from 'qunit';
 import moduleForAcceptance from 'remember/tests/helpers/module-for-acceptance';
 
 import Ember from 'ember';
@@ -40,5 +39,25 @@ test('clicking add new reminder reroutes to reminder/new', function(assert) {
   andThen(function() {
     assert.equal(currentURL(), '/reminders/new');
     assert.equal(Ember.$('input[type="text"]').length, 4);
+  });
+});
+
+test('user can create a new reminder', function(assert) {
+  server.createList('reminder', 5);
+
+  visit('/');
+  click('.create');
+
+  // andThen(function() {
+  //   assert.equal(Ember.$('.reminder-item').length, 5);
+  // });
+
+  fillIn('.title-input', 'test title');
+  fillIn('.body-input', 'test body');
+  fillIn('.date-input', 'test date');
+  click('.add-reminder');
+
+  andThen(function() {
+    assert.equal(Ember.$('.reminder-item').length, 6);
   });
 });
